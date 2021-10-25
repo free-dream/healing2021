0a9c658cad10bccf1f7c09c239fe72665d5c8795
miyasereina <1448472422@qq.com>
2021-10-25 21:46:10 +0800

    fix:修复表迁移问题

--- models/statements/moment_comments.go
+++ models/statements/moment_comments.go
@@ -8,7 +8,7 @@ import (
 )
 
 type MomentComment struct {
-	*gorm.Model
+	gorm.Model
 	UserId   int    `gorm:"default:0;index"`
 	MomentId int    `gorm:"default:0;index"`
 	Comment  string `gorm:"default:''"`
@@ -17,13 +17,14 @@ type MomentComment struct {
 
 func MomentCommentInit() {
 	db := setting.MysqlConn()
-	if !db.HasTable(&Moment{}) {
-		if err := db.CreateTable(&Moment{}).Error; err != nil {
+	if !db.HasTable(&MomentComment{}) {
+		if err := db.CreateTable(&MomentComment{}).Error; err != nil {
 			panic(err)
 		}
 		fmt.Println("Table MomentComment has been created")
 	} else {
-		db.AutoMigrate(&Moment{})
+		db.AutoMigrate(&MomentComment{})
 		fmt.Println("Table MomentComment has existed")
 	}
+	setting.TimeSetting("moment_comment")
 }
