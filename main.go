0e7472f2b5756274f3351efe470cbee08a1960f1
miyasereina <1448472422@qq.com>
2021-11-12 13:51:33 +0800

    fix:矫正假数据

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"git.100steps.top/100steps/healing2021_be/cron"
 	"git.100steps.top/100steps/healing2021_be/models"
-	"git.100steps.top/100steps/healing2021_be/models/statements"
 	"git.100steps.top/100steps/healing2021_be/pkg/setting"
 	"git.100steps.top/100steps/healing2021_be/pkg/tools"
 	"git.100steps.top/100steps/healing2021_be/router"
@@ -14,7 +13,6 @@ import (
 	"log"
 	"strconv"
 	"syscall"
-	"time"
 )
 
 // @Title healing2021
@@ -22,15 +20,12 @@ import (
 // @Description 2021治愈系
 
 func main() {
-	if tools.IsDebug() {
+	/*if tools.IsDebug() {
 		statements.TableClean()
 		time.Sleep(time.Second * 2)
 		models.FakeData()
-	}
+	}*/
 
-	models.AddFakeHomeS()
-	models.AddFakeHomeC()
-	models.AddClassic()
 	models.TableInit()
 
 	routers := router.SetupRouter()
