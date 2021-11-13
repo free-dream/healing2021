b199108e7a4f1d7d72908f37a85c0437fb6186bb
miyasereina <1448472422@qq.com>
2021-11-13 12:54:48 +0800

    fix:增加测试点歌数

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"git.100steps.top/100steps/healing2021_be/cron"
 	"git.100steps.top/100steps/healing2021_be/models"
-	"git.100steps.top/100steps/healing2021_be/models/statements"
 	"git.100steps.top/100steps/healing2021_be/pkg/setting"
 	"git.100steps.top/100steps/healing2021_be/pkg/tools"
 	"git.100steps.top/100steps/healing2021_be/router"
@@ -14,7 +13,6 @@ import (
 	"log"
 	"strconv"
 	"syscall"
-	"time"
 )
 
 // @Title healing2021
@@ -23,13 +21,13 @@ import (
 
 func main() {
 	models.TableInit()
-	if tools.IsDebug() {
+	/*if tools.IsDebug() {
 		statements.TableClean()
 		time.Sleep(time.Second * 2)
 		models.FakeData()
 	}
 	models.AddClassic()
-	models.AddDevotion()
+	models.AddDevotion()*/
 	/*models.AddFakeHomeC()
 	models.AddFakeHomeS()*/
 
