072b2dbc744524df3926b9912d3789ed36932282
miyasereina <1448472422@qq.com>
2021-11-15 07:37:42 +0800

    fix:点赞数重构中

--- cron/cron.go
+++ cron/cron.go
@@ -1,10 +1,8 @@
 package cron
 
 import (
-	"git.100steps.top/100steps/healing2021_be/dao"
 	"git.100steps.top/100steps/healing2021_be/pkg/setting"
 	"github.com/robfig/cron"
-	"strconv"
 )
 
 func CronInit() *cron.Cron {
@@ -17,16 +15,18 @@ func CronInit() *cron.Cron {
 		db := setting.MysqlConn()
 		db.Table("user").Select("selection_num").Update(2)
 	})
-	c.AddFunc("0 */1 * * *", func() {
-		db := setting.MysqlConn()
-		redisCli := setting.RedisConn()
-		rows, _ := db.Exec("select cover_id,count(user_id) as lieks from praise where is_liked=1 group by cover_id;").Rows()
-		like := dao.LikeObj{}
-		defer rows.Close()
-		for rows.Next() {
-			db.ScanRows(rows, &like)
-			redisCli.HSet("healing2021:praise of cover", strconv.Itoa(like.CoverId), like.Likes)
-		}
-	})
+
+	//	c.AddFunc("0 */1 * * *", func() {
+	/*	db := setting.MysqlConn()
+			redisCli := setting.RedisConn()
+			rows, _ := db.Exec("select cover_id,count(user_id) as lieks from praise where is_liked=1 group by cover_id;").Rows()
+			like := dao.LikeObj{}
+			defer rows.Close()
+			for rows.Next() {
+				db.ScanRows(rows, &like)
+				redisCli.HSet("healing2021:praise of cover", strconv.Itoa(like.CoverId), like.Likes)
+			}
+		})
+	*/
 	return c
 }
